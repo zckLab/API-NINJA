f3a52c83b6b4346fa478a18f246ae58d45269007
zklirt <otonielhenrique@icloud.com>
2026-04-09 15:10:39 -0300

    feat: fixing message, maximum capacity and adding CPF

--- src/main/java/com/zcklab/api/dto/NinjaRequestDTO.java
+++ src/main/java/com/zcklab/api/dto/NinjaRequestDTO.java
@@ -5,20 +5,27 @@ import com.zcklab.api.Enums.Category;
 import com.zcklab.api.Enums.Elementals;
 import com.zcklab.api.Enums.Rank;
 import jakarta.validation.constraints.*;
+import org.hibernate.validator.constraints.br.CPF;
 
 import java.time.LocalDate;
 
 public record NinjaRequestDTO(
+
+
         @NotBlank(message = "Name is Required")
-        @Size(min = 3, max = 20, message="Name min characters is 3 and max 12")
+        @Size(min = 3, max = 50, message="Name min characters is 3 and max 50")
         String name,
+
+        @NotBlank
+        @CPF //This is a brazilian validation for the CPF(Brazilian tax identification number)
+        String cpf,
 
         @NotBlank(message = "Email is required")
         @Email(message = "Invalid Email Format")
         String email,
 
         @NotNull(message = "Birth Date is Required")
-        @Past(message = "Birth Date must be in the past")
+        @Past(message = "Birth Date must be in the past") // makes the date not today or in the future (correct use for past or today @PastOrPresent)
         LocalDate birthDate,
 
         @NotNull(message = "Category is Required")
@@ -34,7 +41,7 @@ public record NinjaRequestDTO(
         Rank rank,
 
         @NotBlank(message = "Description is Required")
-        @Size(max = 50, message = "Description should have a max of 50 characters" )
+        @Size(max = 120, message = "Description should have a max of 120 characters" )
         String description
 
 ){}
