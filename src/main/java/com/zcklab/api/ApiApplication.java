3a24112f83bd783d180c3ee8a2aeb21eb69166c0
zklirt <otonielhenrique@icloud.com>
2026-04-29 12:49:27 -0300

    feat: translating comment

--- src/main/java/com/zcklab/api/ApiApplication.java
+++ src/main/java/com/zcklab/api/ApiApplication.java
@@ -10,7 +10,7 @@ public class ApiApplication {
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(ApiApplication.class);
 
-		// Isso aqui ignora o erro do seu arquivo e liga o H2 na marra
+		// This ignores the error in your file and forces the H2 to start
 		app.setDefaultProperties(java.util.Map.of(
 				"spring.h2.console.enabled", "true",
 				"server.port", "8081"
