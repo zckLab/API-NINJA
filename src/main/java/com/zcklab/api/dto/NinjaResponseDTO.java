0344a1eb6fd22357296f4562544b9c54402f8eca
zklirt <otonielhenrique@icloud.com>
2026-05-03 14:49:09 -0300

    feat: new param

--- src/main/java/com/zcklab/api/dto/NinjaResponseDTO.java
+++ src/main/java/com/zcklab/api/dto/NinjaResponseDTO.java
@@ -24,5 +24,6 @@ public record NinjaResponseDTO(
         List<MissionsDTO> missions,
         NearbyVillageDTO nearbyVillages,
         List<ClanDTO> clans,
-        String description
+        String description,
+        Boolean active
 ){}
