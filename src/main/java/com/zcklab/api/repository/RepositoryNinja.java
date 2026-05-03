0b10990fa38adbcebb39184bb5251d5732e648e0
zklirt <otonielhenrique@icloud.com>
2026-05-03 14:48:06 -0300

    feat: new query custom for example, this get only active adults

--- src/main/java/com/zcklab/api/repository/RepositoryNinja.java
+++ src/main/java/com/zcklab/api/repository/RepositoryNinja.java
@@ -4,12 +4,11 @@ import com.zcklab.api.model.Ninja;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Component;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 
-import java.awt.print.Pageable;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -32,5 +31,9 @@ public interface RepositoryNinja extends JpaRepository<Ninja, Long> {
     //This is impractical.
     @Query("SELECT n.email FROM Ninja n")
     List<String> findAllEmails();
+
+    @Query("SELECT n FROM Ninja n WHERE n.age > 18 AND n.active = true")
+    Page<Ninja> findActiveAdults(Pageable pageable);
+
 
 }
